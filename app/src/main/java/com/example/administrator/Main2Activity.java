f354d1934616a4d8ccc32a1dbd4ec1980016a6ef
AFinalStone <602392033@qq.com>
2017-06-20 12:02:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/Main2Activity.java
@@ -0,0 +1,65 @@
+package com.example.administrator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final int width = getWindowManager().getDefaultDisplay().getWidth();
+        final int height = getWindowManager().getDefaultDisplay().getHeight();
+
+        //每10s产生一次点击事件，点击的点坐标为(0.2W - 0.8W,0.2H - 0.8 H),W/H为手机分辨率的宽高.
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                while (true) {
+                    //生成点击坐标
+                    int x = (int) (Math.random() * width * 0.6 + width * 0.2);
+                    int y = (int) (Math.random() * height * 0.6 + height * 0.2);
+                    //利用ProcessBuilder执行shell命令
+                    String[] order = {
+                            "input",
+                            "tap",
+                            "" + x,
+                            "" + y
+                    };
+                    try {
+                        new ProcessBuilder(order).start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //线程睡眠10s
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+
+    /**
+     * 打印点击的点的坐标
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        Toast.makeText(this, "X at " + x + ";Y at " + y, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.administrator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.textView_getPermission).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                execShellCmd("input text  'helloworld!' ");
+            }
+        });
+        findViewById(R.id.textView_sendEvent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                execShellCmd("getevent -p");
+//                execShellCmd("sendevent /dev/input/event0 1 158 1");
+//                execShellCmd("sendevent /dev/input/event0 1 158 0");
+                execShellCmd("input keyevent 3");//home
+//                execShellCmd("input text  'helloworld!' ");
+//                execShellCmd("input tap 168 252");
+//                execShellCmd("input swipe 100 250 200 280");
+            }
+        });
+    }
+
+    /**
+     * 执行shell命令
+     *
+     * @param cmd
+     */
+    private void execShellCmd(String cmd) {
+
+        Toast.makeText(this, cmd,Toast.LENGTH_LONG).show();
+        try {
+            // 申请获取root权限，这一步很重要，不然会没有作用
+            Process process = Runtime.getRuntime().exec("su");
+            // 获取输出流
+            OutputStream outputStream = process.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(
+                    outputStream);
+            dataOutputStream.writeBytes(cmd);
+            dataOutputStream.flush();
+            dataOutputStream.close();
+            outputStream.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+
+}
